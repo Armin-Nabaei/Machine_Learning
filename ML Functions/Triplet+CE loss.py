9dce589f7ba81f23b7d48a0a5019bdfb2b7ae936
Armin Nabaei <armin_nabaei@outlook.com>
2023-12-25 17:17:03 -0500

    Update Triplet+CE loss.py

--- ML Functions/Triplet+CE loss.py
+++ ML Functions/Triplet+CE loss.py
@@ -51,7 +51,7 @@ class MyHuberLoss(Loss): #inherit parent class
 	          positive_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), -1)
 	          negative_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,negative)), -1)
 	          loss_1 = tf.add(tf.subtract(positive_dist, negative_dist), alpha)
-	          loss = tf.reduce_sum(tf.maximum(loss_1,0.0))
+	          loss = tf.reduce_sum(tf.maximum(loss_1,-0.0))
         else:           
             loss= keras.losses.CategoricalCrossentropy()
             loss = loss1(y_true, y_pred)
