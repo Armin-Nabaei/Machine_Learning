e115ef2efba186a14e858e8cd29ebf2fc1d93421
Armin Nabaei <armin_nabaei@outlook.com>
2023-12-25 23:27:27 -0500

    Update Triplet+CE loss.py

--- ML Functions/Triplet+CE loss.py
+++ ML Functions/Triplet+CE loss.py
@@ -1,60 +1,48 @@
 import tensorflow as tf
-from tensorflow.python.framework import ops
-from tensorflow.keras.losses import Loss
-from tensorflow.python.keras.utils import losses_utils
-from tensorflow.python.keras.utils import tf_utils
 from tensorflow import keras
-from tensorflow.keras import backend as K
-from tensorflow.python.ops import gen_math_ops
+import numpy as np
 
-
-class MyHuberLoss(Loss): #inherit parent class
-  
-    #initialize instance attributes
-    def __init__(self, ):
+class MyHuberLoss(keras.losses.Loss):
+    def __init__(self):
         super().__init__()
-
-    #compute loss
+    
     def call(self, y_true, y_pred):
-        v=[]
         threshold1 = 0.4
-        threshold2=0.7
-        #y_pred = tf.convert_to_tensor(y_pred, tf.float32)
-        #alpha=16.0  
+        threshold2 = 0.7
         batch_size = 15
         epsilon = 1.e-9
-        metric = []
         alpha = epsilon
-        #alpha = tf.math.reduce_max ((1-y_true)*y_pred,keepdims=False)
-        negative=(1-y_true)
-        positive=y_pred
-        anchor = y_true
-        class_zero=[1,0,0,0,0,0,0]
-        class_one=[0,1,0,0,0,0,0]
-        class_two=[0,0,1,0,0,0,0]    
-        class_three=[0,0,0,1,0,0,0] 
-        class_four=[0,0,0,0,1,0,0]
-        class_five=[0,0,0,0,0,1,0]
-        class_six=[0,0,0,0,0,0,1]
-        loss=1.0
-          
-           
-        one = tf.constant([1,1,1,1,1,1,1], dtype=tf.float32)
+        y_pred = tf.convert_to_tensor(y_pred, tf.float32)
         
-       
-       
-        alpha = tf.math.reduce_max(tf.multiply((1-y_true),y_pred))
-        t1=y_true.numpy()
+        # Check if y_true is None, and if so, return 0 (or handle it appropriately)
+        if y_true is None:
+            return 0.0
+        
+        y_true = tf.convert_to_tensor(y_true, y_pred.dtype)
+        t1 = y_true.numpy()
+        lossce = keras.losses.CategoricalCrossentropy()(y_true, y_pred)
 
+        if np.all(np.equal(t1, [1, 0, 0, 0, 0, 0, 0])):
+            loss_1 = self.compute_loss(y_true, y_pred)
+            return 0.1 * loss_1 + lossce
 
-        if (tf.reduce_all(tf.equal(t1, class_zero) )).numpy() == False:
-	          positive_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), -1)
-	          negative_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,negative)), -1)
-	          loss_1 = tf.add(tf.subtract(positive_dist, negative_dist), alpha)
-	          trloss = tf.reduce_sum(tf.maximum(loss_1,0.0))
-        else:           
-            loss= keras.losses.CategoricalCrossentropy()
-            celoss = loss1(y_true, y_pred)
+        if np.all(np.equal(t1, [0, 1, 0, 0, 0, 0, 0])):
+            loss_1 = self.compute_loss(y_true, y_pred)
+            return 0.7 * loss_1 + lossce
 
-              
-        return (0.7* trloss) + celoss
+        # Add similar conditions for other cases...
+
+    def compute_loss(self, y_true, y_pred):
+        y_pred = tf.convert_to_tensor(y_pred, tf.float32)
+        y_true = tf.convert_to_tensor(y_true, y_pred.dtype)
+        anchor = y_true
+        negative = 1 - y_true
+        positive = y_pred
+        alpha = tf.math.reduce_max(tf.multiply(negative, y_pred))
+
+        positive_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), -1)
+        negative_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), -1)
+        loss_1 = tf.add(tf.subtract(positive_dist, negative_dist), alpha)
+        loss1 = tf.reduce_sum(tf.maximum(loss_1, 0.0))
+        return loss1
+
